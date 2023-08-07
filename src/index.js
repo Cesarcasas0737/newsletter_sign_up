a8b28d8ad9008f90ee6e35519538d3604ee3db77
cesar <cesarcasas0737@gmail.com>
2023-08-07 13:20:53 -0700

    javascript logit added for email

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+function validateForm() {
+    // Get the email input element
+    var emailInput = document.getElementById('emailInput');
+
+    // Check if the email input is not valid
+    if (!emailInput.validity.valid) {
+        // Display an error message or perform any other action
+        alert('Please enter a valid email address.');
+        return false; // Prevent form submission
+    }
+
+    // If the input is valid, you can submit the form
+    return true;
+}
+
+
 function handleFormSubmission(event){
-    event.preventDeafult();
+
+    event.preventDeafult(); 
+}
+
+function showErrorPopup(message) {
+    var errorMessage = document.querySelector("error-label");
+
+    errorMessage.innerText = message;
+    errorPopup.style.display = 'block';
 }
